7c38c4525ed5f53d22b866c48a40101457097aff
Murray2015 <murrayhoggett@gmail.com>
2020-02-28 16:39:07 +0000

    Got evolutions working

--- src/components/PokemonDisplay/PokemonDisplay.js
+++ src/components/PokemonDisplay/PokemonDisplay.js
@@ -2,20 +2,37 @@ import React from "react";
 import css from "./PokemonDisplay.module.css";
 
 function PokemonDisplay({ result }) {
-  console.log(result);
+  var evolvesTo = {};
+  try {
+    evolvesTo = JSON.parse(result.evolutions);
+    console.log("evo", evolvesTo.to);
+  } catch (err) {
+    console.log(err);
+  }
   return (
-    <div>
-      <div className={css.pokemonTextCard}>
-        <img
-          className={css.pokemonImg}
-          src={result.img_url}
-          alt={result.name}
-        />
-        <h2 className={css.pokemonH1}>{result.name}</h2>
-        <p>ID: {result.id}</p>
-        <p>Types: {result.types}</p>
-        <p>{result.description}</p>
-      </div>
+    <div className={css.pokemonTextCard + " " + css.rotateCenter}>
+      <img className={css.pokemonImg} src={result.img_url} alt={result.name} />
+      <h2 className={css.pokemonH1}>{result.name}</h2>
+      <p>
+        <span style={{ fontWeight: "bold" }}>ID: </span>
+        {result.id}
+      </p>
+      <ul>
+        <span style={{ fontWeight: "bold" }}>Types:</span>
+        {result.types.map(type => (
+          <span>{` ${type} `}</span>
+        ))}
+      </ul>
+      {evolvesTo.to && (
+        <p>
+          <span style={{ fontWeight: "bold" }}>Evolves to: </span>
+          {evolvesTo.to}
+        </p>
+      )}
+      <p>
+        <span style={{ fontWeight: "bold" }}>Description: </span>
+        {result.description}
+      </p>
     </div>
   );
 }
